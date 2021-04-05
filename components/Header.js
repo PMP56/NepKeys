3cc876c11667c66a32201e4675833d8492dea2eb
PMP56 <prashannapaudel2@gmail.com>
2021-04-05 17:15:56 +0545

    Small changes

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <div className={styles.logoBox}>
                 <Logo />
                 <h1 className={styles.title}>NepKeys</h1>
-                <p className={styles.hiddenText}>NepKeys is a free online Nepali typing practice site which provides you a better environment to learn typing and analytics which help you to track your progess.</p>
+                <h1 className={styles.hiddenText}>NepKeys is a free online Nepali typing practice site which provides you a better environment to learn typing and analytics which help you to track your progess.</h1>
             </div>
             <h2 className={styles.description}>Practice Nepali Typing Online</h2>
         </header>
