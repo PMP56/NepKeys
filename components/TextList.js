9d30acd27b2326a642d42c1a746a0d7a5874d8f0
PMP56 <prashannapaudel2@gmail.com>
2021-04-12 23:21:21 +0545

    Texts added

--- components/TextList.js
+++ components/TextList.js
@@ -1,5 +1,14 @@
 export const TextList = [
     "Text to type",
-    "This is Prashanna Mani Paudel",
-    ""
+    "tire ter tire irreline tire irrel err ner retine err ter rel",
+    "trient err tire relier ner irrient ner ret irrientine rent tere",
+    "ter irrient tre tire ner irreline err tire ter err ter err tire",
+    "ner tree ner tir irriet ter irrienten tirine trient tre ner",
+    "ren tire ner errent trient tre irrentle tret erren ner ere irient",
+    "irrent irren trient irrel irrie ner ter err ner ter ner err",
+    "irrent irrin errel erren irrel irrient err tire errelie",
+    "ter irrent errien ter irrient err ner ter tree tire trient",
+    "ner erreline errient ter tire ner ter err irrelie ter relier",
+    "irrient irrieter tire errent irrelie irrient trient err ter",
+    "a student should never confine himself to his schoolbooks only.",
 ]
