03f43e1b18880cf01bcf592975acd03b710d69e8
PMP56 <prashannapaudel2@gmail.com>
2021-04-15 23:51:38 +0545

    GTM link added

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
         return (
             <Html lang='en'>
                 <Head>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4CSB9TMNWN"></script>
                     <script dangerouslySetInnerHTML={{
                         __html: `window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
