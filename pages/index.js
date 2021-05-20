a98990bd5ac7480904983c630f6a170e90b9166e
PMP56 <prashannapaudel2@gmail.com>
2021-05-21 01:40:58 +0545

    Ads changed

--- pages/index.js
+++ pages/index.js
@@ -105,20 +105,22 @@ export default function Home(props) {
     <main className={`whole ${styles.container} ${(theme == 'light') ? styles.light : (theme == 'dark') ? styles.dark : styles.color}`} tabIndex="0" onKeyDown={buttonPress} onKeyUp={buttonRelease}>
       <Header lang={props.lang} />
       <Body lang={props.lang} fonts={props.fonts} theme={theme} clickSlider={clickSlider}>
-        {props.children ? props.children : <div className={styles.information}>
-          <h1 className={styles.informationHeader}>नेपाली - Nepali Typing Practice Online, Nepali Keyboard Layout, Nepali Typing Tutor and Typing Test Online</h1>
-          <h4 className={styles.informationText}>NepKeys is an online typing practice and typing test site with different themes and layouts of keyboard that helps you improve your typing skill with the analytics tool integrated inside the website. You can learn to type in various languages such as English, Nepali, Hindi and Newari with different keyboard for each language. It is absolutely free to use and guides you along your journey of learning fast and accuate typing. It calculates your accuracy, total letter and words, words per minute i.e typing speed and more importantly shows different graphs of how you improved throughout the process. It also saves your progress so that you can continue on where you last left. </h4>
-          <h1 className={styles.features}>
-            Feature of NepKeys
+        {props.children ? props.children : <div className={styles.wholeInformation}>
+          <div className={styles.information}>
+            <h1 className={styles.informationHeader}>नेपाली - Nepali Typing Practice Online, Nepali Keyboard Layout, Nepali Typing Tutor and Typing Test Online</h1>
+            <h4 className={styles.informationText}>NepKeys is an online typing practice and typing test site with different themes and layouts of keyboard that helps you improve your typing skill with the analytics tool integrated inside the website. You can learn to type in various languages such as English, Nepali, Hindi and Newari with different keyboard for each language. It is absolutely free to use and guides you along your journey of learning fast and accuate typing. It calculates your accuracy, total letter and words, words per minute i.e typing speed and more importantly shows different graphs of how you improved throughout the process. It also saves your progress so that you can continue on where you last left. </h4>
+            <h1 className={styles.features}>
+              Feature of NepKeys
             </h1>
-          <ul className={styles.featuresList}>
-            <li><h1 className={styles.featuresNumber}>Online Nepali Typing Practice Anytime for Free</h1></li>
-            <li><h1 className={styles.featuresNumber}>Nepali Typing Tutor with Nepali Keyboard Layout Available</h1></li>
-            <li><h1 className={styles.featuresNumber}>Interchangeable Language to Practice Typing in Any Language</h1></li>
-            <li><h1 className={styles.featuresNumber}>Typing Test to Calcualte Words Per Minute and Accuracy Percentage</h1></li>
-            <li><h1 className={styles.featuresNumber}>Graphs and Charts to Show Your Progress Throughout and Compare with Other People.</h1></li>
-            <li><h1 className={styles.featuresNumber}>Next Generation Easy to use Typeshala with Scientific Calculation and Accurate Result.</h1></li>
-          </ul>
+            <ul className={styles.featuresList}>
+              <li><h1 className={styles.featuresNumber}>Online Nepali Typing Practice Anytime for Free</h1></li>
+              <li><h1 className={styles.featuresNumber}>Nepali Typing Tutor with Nepali Keyboard Layout Available</h1></li>
+              <li><h1 className={styles.featuresNumber}>Interchangeable Language to Practice Typing in Any Language</h1></li>
+              <li><h1 className={styles.featuresNumber}>Typing Test to Calcualte Words Per Minute and Accuracy Percentage</h1></li>
+              <li><h1 className={styles.featuresNumber}>Graphs and Charts to Show Your Progress Throughout and Compare with Other People.</h1></li>
+              <li><h1 className={styles.featuresNumber}>Next Generation Easy to use Typeshala with Scientific Calculation and Accurate Result.</h1></li>
+            </ul>
+          </div>
         </div>}
       </Body>
     </main>
--- pages/lang/in.js
+++ pages/lang/in.js
@@ -17,20 +17,23 @@ const Body = () => {
                 <link rel="canonical" href="https://www.nepkeys.site/lang/in" />
             </Head>
             <BodyPage lang="Hindi" fonts={{ "text": "40px", "upperKey": "19px", "lowerKey": "24px" }} >
-                <div className={styles.information}>
-                    <h1 className={styles.informationHeader}>हिन्दी - Hindi Typing Practice Online, Hindi Keyboard Layout, Hindi Typing Tutor and Typing Test Online</h1>
-                    <h4 className={styles.informationText}>NepKeys is an online typing practice and typing test site with different themes and layouts of keyboard that helps you improve your typing skill with the analytics tool integrated inside the website. You can learn to type in various languages such as English, Nepali, Hindi and Newari with different keyboard for each language. It is absolutely free to use and guides you along your journey of learning fast and accuate typing. It calculates your accuracy, total letter and words, words per minute i.e typing speed and more importantly shows different graphs of how you improved throughout the process. It also saves your progress so that you can continue on where you last left. </h4>
-                    <h1 className={styles.features}>
-                        Feature of NepKeys
-                    </h1>
-                    <ul className={styles.featuresList}>
-                        <li><h1 className={styles.featuresNumber}>Online Hindi Typing Practice Anytime for Free</h1></li>
-                        <li><h1 className={styles.featuresNumber}>Hindi Typing Tutor with Hindi Keyboard Layout Available</h1></li>
-                        <li><h1 className={styles.featuresNumber}>Interchangeable Language to Practice Typing in Any Language</h1></li>
-                        <li><h1 className={styles.featuresNumber}>Typing Test to Calcualte Words Per Minute and Accuracy Percentage</h1></li>
-                        <li><h1 className={styles.featuresNumber}>Graphs and Charts to Show Your Progress Throughout and Compare with Other People.</h1></li>
-                        <li><h1 className={styles.featuresNumber}>Next Generation Easy to use Typeshala with Scientific Calculation and Accurate Result.</h1></li>
-                    </ul>
+                <div className={styles.wholeInformation}>
+                    <div className={styles.information}>
+
+                        <h1 className={styles.informationHeader}>हिन्दी - Hindi Typing Practice Online, Hindi Keyboard Layout, Hindi Typing Tutor and Typing Test Online</h1>
+                        <h4 className={styles.informationText}>NepKeys is an online typing practice and typing test site with different themes and layouts of keyboard that helps you improve your typing skill with the analytics tool integrated inside the website. You can learn to type in various languages such as English, Nepali, Hindi and Newari with different keyboard for each language. It is absolutely free to use and guides you along your journey of learning fast and accuate typing. It calculates your accuracy, total letter and words, words per minute i.e typing speed and more importantly shows different graphs of how you improved throughout the process. It also saves your progress so that you can continue on where you last left. </h4>
+                        <h1 className={styles.features}>
+                            Feature of NepKeys
+                        </h1>
+                        <ul className={styles.featuresList}>
+                            <li><h1 className={styles.featuresNumber}>Online Hindi Typing Practice Anytime for Free</h1></li>
+                            <li><h1 className={styles.featuresNumber}>Hindi Typing Tutor with Hindi Keyboard Layout Available</h1></li>
+                            <li><h1 className={styles.featuresNumber}>Interchangeable Language to Practice Typing in Any Language</h1></li>
+                            <li><h1 className={styles.featuresNumber}>Typing Test to Calcualte Words Per Minute and Accuracy Percentage</h1></li>
+                            <li><h1 className={styles.featuresNumber}>Graphs and Charts to Show Your Progress Throughout and Compare with Other People.</h1></li>
+                            <li><h1 className={styles.featuresNumber}>Next Generation Easy to use Typeshala with Scientific Calculation and Accurate Result.</h1></li>
+                        </ul>
+                    </div>
                 </div>
             </BodyPage>
         </Fragment>
